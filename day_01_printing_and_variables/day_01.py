840be0353d1eada8026b4d62f8f6b0e5f14ba744
Hotteok Mind <52848569+sliverz6@users.noreply.github.com>
2021-02-05 18:58:48 +0900

    2021-02-05 Study

---
+++ day_01_printing_and_variables/day_01.py
@@ -0,0 +1,23 @@
+# day_01.py
+
+# print() 함수 - 콘솔에 출력
+print("Hello world!")
+
+# 줄바꿈
+print("Hello\nworld!")
+
+# 문자열 연결
+print("Hello" + " " + "world!")
+
+# input() 함수 - 사용자 입력 받기
+print(input("What is your name? "))
+
+# len() 함수 - 문자열 길이 구하기
+print(len("Hello"))
+
+# 변수
+name = input("What is your name? ")
+print(name)
+
+name = "Lee"  # 재할당
+print(name)
